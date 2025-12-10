d497df1bf9d12ec3d688af207c7f48e4de5f097c
Samuel Boyden <sboyden@certosoftware.com>
2025-12-10 11:57:08 +0000

    Use SwiftUIIntrospect to get view controller

--- Package.swift
+++ Package.swift
@@ -15,11 +15,12 @@ let package = Package(
         .library(
             name: "SwiftUIBackports",
             targets: ["SwiftUIBackports"]
-        ),
+        )
     ],
     dependencies: [
         .package(url: "https://github.com/shaps80/SwiftBackports", from: "26.0.1"),
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+        .package(url: "https://github.com/siteline/swiftui-introspect", "1.3.0"..<"27.0.0"),
     ],
     targets: [
         .target(
--- Sources/SwiftUIBackports/Shared/Toolbar/ToolbarBackgroundModifier.swift
+++ Sources/SwiftUIBackports/Shared/Toolbar/ToolbarBackgroundModifier.swift
@@ -1,97 +1,106 @@
 import SwiftBackports
 
 #if os(iOS)
-import SwiftUI
+    import SwiftUI
+    @_spi(Advanced) import SwiftUIIntrospect
 
-internal struct ToolbarBackgroundModifier: ViewModifier {
-    @Environment(\.toolbarViews) private var toolbarViews
-    @Environment(\.toolbarVisibility) private var toolbarVisibility
+    internal struct ToolbarBackgroundModifier: ViewModifier {
+        @Environment(\.toolbarViews) private var toolbarViews
+        @Environment(\.toolbarVisibility) private var toolbarVisibility
 
-    @Backport.StateObject private var wrapper: ControllerWrapper = .init()
+        @Backport.StateObject private var wrapper: ControllerWrapper = .init()
 
-    func body(content: Content) -> some View {
-        content
-            .ancestor(forType: UIViewController.self) { proxy in
-                wrapper.controller = proxy.instance
+        func body(content: Content) -> some View {
+            content
+                .introspect(.viewController, on: .iOS(.v13...)) { viewController in
+                    wrapper.controller = viewController
+                }
+                .backport.task(id: wrapper.controller) {
+                    updateNavigationBar()
+                    updateBottomBar()
+                    updateTabBar()
+                }
+        }
+
+        private func updateNavigationBar() {
+            guard toolbarVisibility.navigationBar != nil || toolbarViews.navigationBar != nil else {
+                return
             }
-            .backport.task {
-                updateNavigationBar()
-                updateBottomBar()
-                updateTabBar()
+            guard let bar = wrapper.controller?.navigationController?.navigationBar else { return }
+            let appearance = UINavigationBarAppearance()
+
+            if let visibilty = toolbarVisibility.navigationBar, visibilty == .hidden {
+                appearance.configureWithTransparentBackground()
+            } else if let view = toolbarViews.navigationBar {
+                appearance.backgroundImage =
+                    Backport.ImageRenderer(
+                        content:
+                            view
+                            .frame(width: bar.bounds.width, height: bar.bounds.height)
+                            .edgesIgnoringSafeArea(.all)
+                    ).uiImage
+            } else {
+                appearance.configureWithDefaultBackground()
             }
-    }
 
-    private func updateNavigationBar() {
-        guard toolbarVisibility.navigationBar != nil || toolbarViews.navigationBar != nil else { return }
-        guard let bar = wrapper.controller?.navigationController?.navigationBar else { return }
-        let appearance = UINavigationBarAppearance()
-
-        if let visibilty = toolbarVisibility.navigationBar, visibilty == .hidden {
-            appearance.configureWithTransparentBackground()
-        } else if let view = toolbarViews.navigationBar {
-            appearance.backgroundImage = Backport.ImageRenderer(
-                content: view
-                    .frame(width: bar.bounds.width, height: bar.bounds.height)
-                    .edgesIgnoringSafeArea(.all)
-            ).uiImage
-        } else {
-            appearance.configureWithDefaultBackground()
-        }
-
-        bar.standardAppearance = appearance
-        bar.compactAppearance = appearance
-        bar.scrollEdgeAppearance = appearance
-        if #available(iOS 15.0, *) {
-            bar.compactScrollEdgeAppearance = appearance
-        }
-    }
-
-    private func updateBottomBar() {
-        guard toolbarVisibility.bottomBar != nil || toolbarViews.bottomBar != nil else { return }
-        guard let bar = wrapper.controller?.navigationController?.toolbar else { return }
-        let appearance = UIToolbarAppearance()
-
-        if let visibilty = toolbarVisibility.bottomBar, visibilty == .hidden {
-            appearance.configureWithTransparentBackground()
-        } else {
-            appearance.configureWithDefaultBackground()
-        }
-
-        bar.standardAppearance = appearance
-        bar.compactAppearance = appearance
-        if #available(iOS 15.0, *) {
+            bar.standardAppearance = appearance
+            bar.compactAppearance = appearance
             bar.scrollEdgeAppearance = appearance
+            if #available(iOS 15.0, *) {
+                bar.compactScrollEdgeAppearance = appearance
+            }
         }
-        if #available(iOS 15.0, *) {
-            bar.compactScrollEdgeAppearance = appearance
+
+        private func updateBottomBar() {
+            guard toolbarVisibility.bottomBar != nil || toolbarViews.bottomBar != nil else {
+                return
+            }
+            guard let bar = wrapper.controller?.navigationController?.toolbar else { return }
+            let appearance = UIToolbarAppearance()
+
+            if let visibilty = toolbarVisibility.bottomBar, visibilty == .hidden {
+                appearance.configureWithTransparentBackground()
+            } else {
+                appearance.configureWithDefaultBackground()
+            }
+
+            bar.standardAppearance = appearance
+            bar.compactAppearance = appearance
+            if #available(iOS 15.0, *) {
+                bar.scrollEdgeAppearance = appearance
+            }
+            if #available(iOS 15.0, *) {
+                bar.compactScrollEdgeAppearance = appearance
+            }
+        }
+
+        private func updateTabBar() {
+            guard toolbarVisibility.tabBar != nil || toolbarViews.tabBar != nil else { return }
+            guard let bar = wrapper.controller?.tabBarController?.tabBar else { return }
+            let appearance = UITabBarAppearance()
+
+            if let visibilty = toolbarVisibility.tabBar, visibilty == .hidden {
+                appearance.configureWithTransparentBackground()
+            } else if let view = toolbarViews.tabBar {
+                appearance.backgroundImage =
+                    Backport.ImageRenderer(
+                        content:
+                            view
+                            .frame(width: bar.bounds.width, height: bar.bounds.height)
+                            .edgesIgnoringSafeArea(.all)
+                    ).uiImage
+            } else {
+                appearance.configureWithDefaultBackground()
+            }
+
+            bar.standardAppearance = appearance
+            if #available(iOS 15.0, *) {
+                bar.scrollEdgeAppearance = appearance
+            }
         }
     }
 
-    private func updateTabBar() {
-        guard toolbarVisibility.tabBar != nil || toolbarViews.tabBar != nil else { return }
-        guard let bar = wrapper.controller?.tabBarController?.tabBar else { return }
-        let appearance = UITabBarAppearance()
-
-        if let visibilty = toolbarVisibility.tabBar, visibilty == .hidden {
-            appearance.configureWithTransparentBackground()
-        } else if let view = toolbarViews.tabBar {
-            appearance.backgroundImage = Backport.ImageRenderer(
-                content: view
-                    .frame(width: bar.bounds.width, height: bar.bounds.height)
-                    .edgesIgnoringSafeArea(.all)
-            ).uiImage
-        } else {
-            appearance.configureWithDefaultBackground()
-        }
-
-        bar.standardAppearance = appearance
-        if #available(iOS 15.0, *) {
-            bar.scrollEdgeAppearance = appearance
-        }
+    private final class ControllerWrapper: ObservableObject {
+        weak var controller: UIViewController?
     }
-}
-
-private final class ControllerWrapper: ObservableObject {
-    weak var controller: UIViewController?
-}
 #endif
